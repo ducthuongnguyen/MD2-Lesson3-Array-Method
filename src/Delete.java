a31d2ba763443d80ee3a837d1224de7aa2f0edc9
ducthuongnguyen <ducthuong.nguyen241@gmail.com>
2022-03-26 16:18:55 +0700

    update

---
+++ src/Delete.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+public class Delete {
+    public static void main(String[] as) {
+        Scanner scanner = new Scanner(System.in);
+        int input;
+        int sizeD;
+        int size;
+
+        System.out.println("Nhập vào size: "); //Size của mảng number tạo ban đầu
+        size = scanner.nextInt();
+        int[] arrNumber = new int[size];
+
+        for (int i = 0; i < arrNumber.length; i++) { //Vòng lặp tạo mảng
+            System.out.println("Nhập vào phần tử:  " + i);
+            arrNumber[i] = scanner.nextInt();
+        }
+        for (int number : arrNumber) { //in ra mảng ban đầu
+            System.out.print(" " + number);
+        }
+        System.out.println();
+        System.out.println("Nhập vào phần tử cần xóa : ");
+        input = scanner.nextInt();
+        sizeD = sizeArrDuplicate(arrNumber, input);
+        int sizeAfterDelete = size - sizeD;
+        int count = 0;
+        int[] newArr = new int[sizeAfterDelete];
+        for (int i = 0; i < arrNumber.length; i++) { //xử lí logic xóa gán lại mảng mới
+            if(count<=sizeAfterDelete){
+                if(arrNumber[i]!=input){
+                    newArr[count] = arrNumber[i];
+                    count++;
+                } else {
+                    continue;
+                }
+            }
+        }
+        System.out.println("Mảng sau khi xóa: ");
+        for (int element : newArr) { //in mảng sau khi xóa
+            System.out.print(" " + element);
+        }
+    }
+    static int sizeArrDuplicate(int[] arr, int input) { //Hàm lấy ra size của mảng trùng
+        int sizeD = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (input == arr[i]) {
+                sizeD++;
+            }
+        }
+        return sizeD;
+    }
+}
